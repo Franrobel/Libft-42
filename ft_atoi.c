2bb59eb5a057d12255f36dec04759c752a832048
Francisco Rodriguez Belluati <franrobelluati@gmail.com>
2025-05-20 01:05:14 +0200

    way from char to int

--- ft_atoi.c
+++ ft_atoi.c
@@ -25,31 +25,21 @@ int ft_atoi(const char *str)
     {
         if (str[i] >= '0' && str[i] <= '9')
         {
-            printf(" num j y i es %d %d\n", j, i);
             nums[j] = str[i];
             j++;
-            // printf(" X i+1 es %d\n", i+1);
             if (str[i + 1] < '0' || str[i + 1] > '9')
             {
-                // printf("nums '%s'\n", nums);
                 numslen = ft_strlen(nums);
-                printf("numslen %zu\n", numslen);
-                while (x < numslen - 1)
+                value = 0;
+                while (x < numslen)
                 {
-                    // hacer un char * con 0 tantos ceros como tenga numslen
-                    // si nums es 983 el leng es 3 hago ese char "000" por lo tanto si sumo nums + "000" 
-                    // me dara el int
-                    printf("nums[%zu] es %c\nnums[%zu] es %c\n", x, nums[x], x + 1, nums[x + 1]);
-                    printf("value es %d\n", (nums[x] - '0') * 10 + (nums[x + 1] - '0'));
-                    value = (nums[x] - '0') * 10 + (nums[x + 1] - '0');
+                    value = value * 10 + (nums[x] - '0');
                     x++;
                 }
                 return (value);
             }
         }
-        //  printf("str[%d] is %c\n", i, str[i]);
         i++;
     }
-    // printf("nums %s\n", nums);
     return (0);
 }
