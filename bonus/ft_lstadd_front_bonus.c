7b61f8d6f096ba9569582a5be90a1b7ec24e187c
Francisco Rodriguez Belluati <franrobelluati@gmail.com>
2025-05-29 16:55:28 +0200

    adding 2nd bonus function

---
+++ bonus/ft_lstadd_front_bonus.c
@@ -0,0 +1,16 @@
+#include "../libft.h"
+
+void ft_lstadd_front(t_list **lst, t_list *new)
+{
+
+}
+/*
+Parámetros lst: la dirección de un puntero al primer nodo de
+una lista.
+new: un puntero al nodo que añadir al principio de
+la lista.
+Valor devuelto Nada
+Funciones autorizadas
+Ninguna
+Descripción Añade el nodo ’new’ al principio de la lista ’lst’.
+*/
